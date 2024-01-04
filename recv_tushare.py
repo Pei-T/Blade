0f65734aac077885d982da060929e82d0daa8248
Pei.tang <pea.tang@qq.com>
2024-01-04 20:43:06 +0800

    fix time error

--- recv_tushare.py
+++ recv_tushare.py
@@ -4,15 +4,10 @@ import time
 
 
 tushare_token = 'db11129040085642ce635dd20923d2aa6bc5bc04dd07b42afc271ad4'
-# df = ts.get_realtime_quotes('600203')  # Single stock symbol
-# df[['code', 'name', 'price', 'bid', 'ask', 'volume', 'amount', 'time']]
-# print(df)
-
 
 class RecvTushareOnlien(RecvMsg):
     def __init__(self, code) -> None:
         self.code = code
-        self.last_time = datetime.now()
 
     def Initialization(self) -> bool:
         return True
